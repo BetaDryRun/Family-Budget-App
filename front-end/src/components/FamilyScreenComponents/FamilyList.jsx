07a784a81ed8052c6edf58985710f62d3a58401b
naman12g <naman54g@gmail.com>
2021-07-23 22:47:47 +0530

    Family cards done

--- front-end/src/components/FamilyScreenComponents/FamilyList.jsx
+++ front-end/src/components/FamilyScreenComponents/FamilyList.jsx
@@ -1,51 +1,70 @@
-import React from 'react';
-import { Button, View, TouchableOpacity, FlatList } from 'react-native';
-import { DummyFamily } from '../../dummyData/Data';
-import { Container, Content, Card, CardItem, Text } from 'native-base';
+import React from "react";
+import { Button, View, TouchableOpacity, FlatList } from "react-native";
+import { DummyFamily } from "../../dummyData/Data";
+import {
+  VStack,
+  HStack,
+  Avatar,
+  Image,
+  Text,
+  NativeBaseProvider,
+  AspectRatio,
+  Center,
+  Box,
+  Stack,
+  Heading,
+} from "native-base";
 
-const FamilyList = ({navigation}) => {
-
-    const renderItem = () => {
-        <Container>
-          <Content>
-            <Card>
-              <CardItem header>
-                <Text>Card Header</Text>
-              </CardItem>
-
-              <CardItem>
-                <Text>//Amount in family wallet</Text>
-              </CardItem>
-
-              <CardItem>
-                <Text>//Description</Text>
-              </CardItem>
-
-            </Card>
-          </Content>
-        </Container>;
-    };
+const FamilyList = ({ navigation }) => {
+    const changeBal = (item) => {
+        let bal = 0;
+        item.map((key) => {
+            console.log(key);
+            bal = bal + key.remainingBudget;
+        })
+        return (
+            <Text color="gray.400">{bal}</Text>
+        );
+    }
+  const renderItem = ({ item, index }) => {
     return (
-  <View style={{ flex: 1, alignItems: "center"}}>
-            <Text>List of Families Page</Text>
-            {console.log('Naman')}
-    <FlatList
-      data={DummyFamily}
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{
-        paddingHorizontal: 16,
-        paddingBottom: 16,
-        marginTop: 16,
-      }}
-      keyExtractor={({ item, index }) => String(index)}
-      renderItem={renderItem}
-    ></FlatList>
-    <Button
-      title="Go to Family 1"
-      onPress={() => navigation.navigate("Family")}
-    />
-  </View>
-);
-}
+      <Box bg="white" shadow={5} rounded="lg" maxWidth="100%" mt={5}>
+        <Stack space={4} m={4}>
+          <Text color="gray.400">
+            No Of Members:{" "}
+            {item.members.length + item.members.length + item.members.length}
+          </Text>
+          <Heading size={["md", "lg", "md"]}>{item.name}</Heading>
+          <Text lineHeight={[5, 5, 7]} noOfLines={[4, 4, 2]} color="gray.700">
+            {item.description}
+          </Text>
+          <Text color="gray.400">
+            Family Account Balance: {changeBal(item.membersBudgets)}/{item.budget}
+          </Text>
+        </Stack>
+      </Box>
+    );
+  };
+  return (
+      <View style={{ flex: 1 }}>
+          {/* <TouchableOpacity onPress={()} */}
+      <FlatList
+        data={DummyFamily}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+          marginTop: 16,
+        }}
+        keyExtractor={({ item, index }) => String(index)}
+        renderItem={renderItem}
+      ></FlatList>
+      <Button
+        title="Go to Family 1"
+        onPress={() => navigation.navigate("Family")}
+      />
+    </View>
+  );
+};
 
 export default FamilyList;
--- front-end/src/dummyData/Data.js
+++ front-end/src/dummyData/Data.js
@@ -3,14 +3,14 @@ export const DummyFamily = [
     id: "132415",
     name: "Family 1",
     link: "https://share",
-    description: "About the family",
+    description: "About the family argarga aergarg rga agaraga",
     members: ["mid1", "mid2", "mid3"],
     admins: ["aid1", "aid2", "aid3…"],
     seasoned: ["sid1", "sid2", "sid3…"],
     walletId: "id", // will be same as userID
     membersBudgets: [
-      { id1: "mid1", budget: 123, remainingBudget: 14 },
-      { id2: "mid2", budget: 13, remainingBudget: 51 },
+      { id1: "mid1", budget: 123, remainingBudget: 134 },
+      { id2: "mid2", budget: 13, remainingBudget: 514 },
     ],
     iterationDuration: "Daily",
     // budget: double   → this should be equal to the sum of all the membersBudgets.
@@ -21,18 +21,36 @@ export const DummyFamily = [
     id: "151515",
     name: "Family 2",
     link: "https://share2",
-    description: "About the family 2",
+    description: "About the family 2 agarga agrarg th gag arfsd",
     members: ["mid1", "mid2", "mid3"],
     admins: ["aid1", "aid2", "aid3…"],
     seasoned: ["sid1", "sid2", "sid3…"],
     walletId: "id", // will be same as userID
     membersBudgets: [
-      { id1: "mid1", budget: 515, remainingBudget: 52 },
-      { id2: "mid2", budget: 566, remainingBudget: 67 },
+      { id1: "mid1", budget: 515, remainingBudget: 525 },
+      { id2: "mid2", budget: 566, remainingBudget: 676 },
     ],
     iterationDuration: "Monthly",
     // budget: double   → this should be equal to the sum of all the membersBudgets.
     budget: 10000,
+    tags: ["tag1", "tag2"],
+  },
+  {
+    id: "133753",
+    name: "Family 3",
+    link: "https://share3",
+    description: "About the family 2 agarhsgdddddddd argafgdfg aregfgafgadg agrg",
+    members: ["mid1", "mid2", "mid3"],
+    admins: ["aid1", "aid2", "aid3…"],
+    seasoned: ["sid1", "sid2", "sid3…"],
+    walletId: "id", // will be same as userID
+    membersBudgets: [
+      { id1: "mid1", budget: 515, remainingBudget: 365 },
+      { id2: "mid2", budget: 566, remainingBudget: 4366 },
+    ],
+    iterationDuration: "Monthly",
+    // budget: double   → this should be equal to the sum of all the membersBudgets.
+    budget: 6000,
     tags: ["tag1", "tag2"],
   },
 ];
