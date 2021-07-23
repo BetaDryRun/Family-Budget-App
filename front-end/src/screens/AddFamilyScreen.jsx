f50df302d5378e7f9774a9e8a55ab680ed115945
naman12g <naman54g@gmail.com>
2021-07-24 01:44:07 +0530

    Except dropdown add family Complete

--- front-end/src/screens/AddFamilyScreen.jsx
+++ front-end/src/screens/AddFamilyScreen.jsx
@@ -22,11 +22,14 @@ import { useForm, Controller } from "react-hook-form";
 
 const AddFamilyScreen = (props) => {
   const { control, handleSubmit, errors } = useForm();
+  const onChangeText = (value) => {
+    console.log("Dropdown value ", value);
+  }
   const onSubmit = (data) => {
     console.log("submiting with ", data);
   };
   return (
-    <View bg={"fi.400"}>
+    <View bg={"fi.400"} h="100%">
       {/* <Text style={styles.textStyle}>AddFamilyScreen Page</Text> */}
       {/* //name description iterationDuration budget */}
       <VStack space={5} mt={10} w="90%" pb={10}>
@@ -45,38 +48,49 @@ const AddFamilyScreen = (props) => {
             Family Description:
           </FormControl.Label>
           <Input multiline={true} numberOfLines={4} />
-            {/* <FormControl.Label>Fav language:</FormControl.Label>
-            <Controller
-              control={control}
-              render={({ onChange, value }) => (
-                <Select
-                  placeholder="Choose Iteration Period"
-                  selectedValue={value}
-                  width={150}
-                  onValueChange={(itemValue: string) => {
-                    onChange(itemValue);
-                  }}
-                  selectedItemBg={"teal.400"}
-                  dropdownOpenIcon={
-                    <Icon name="arrow-drop-up" type="MaterialIcons" size={6} />
-                  }
-                  dropdownCloseIcon={
-                    <Icon
-                      name="arrow-drop-down"
-                      type="MaterialIcons"
-                      size={6}
-                    />
-                  }
-                >
-                  <Select.Item label="JavaScript" value="js" />
-                  <Select.Item label="TypeScript" value="ts" />
-                  <Select.Item label="Java" value="java" />
-                </Select>
-              )}
-              name="language"
-              rules={{ required: "Field is required" }}
-              defaultValue="js"
-            /> */}
+        </FormControl>
+        <FormControl mr={5} ml={5}>
+          <FormControl.Label
+            _text={{ color: "fi.50", fontSize: "sm", fontWeight: 600 }}
+          >
+            Iteration Period:
+          </FormControl.Label>
+          <Controller
+            control={control}
+            render={({ onChangeText, value }) => (
+              <Select
+                placeholder="Choose Iteration Period"
+                selectedValue={value}
+                width={"100%"}
+                onValueChange={(itemValue: String) => {
+                  onChangeText(itemValue);
+                }}
+                selectedItemBg={"fi.300"}
+                dropdownOpenIcon={
+                  <Icon name="arrow-drop-up" type="MaterialIcons" size={6} />
+                }
+                dropdownCloseIcon={
+                  <Icon name="arrow-drop-down" type="MaterialIcons" size={6} />
+                }
+              >
+                <Select.Item label="Daily" value="dy" />
+                <Select.Item label="Weekly" value="wy" />
+                <Select.Item label="Monthly" value="my" />
+                <Select.Item label="Yearly" value="yy" />
+              </Select>
+            )}
+            name="Iteration Time"
+            rules={{ required: "Field is required" }}
+            defaultValue="my"
+          />
+        </FormControl>
+        <FormControl mr={5} ml={5}>
+          <FormControl.Label
+            _text={{ color: "fi.50", fontSize: "sm", fontWeight: 600 }}
+          >
+            Family Budget For Selected Time Period:
+          </FormControl.Label>
+          <Input />
         </FormControl>
         <Button onPress={handleSubmit(onSubmit)} color="fi.300" ml={10}>
           Create Group
