179b8fbccdb3fd878366b0d4fc8e26b10ea0733b
sarthak.das <sarthak.das@naukri.com>
2021-07-25 00:34:46 +0530

    Profiles in progress...

--- front-end/src/screens/ProfileScreen.jsx
+++ front-end/src/screens/ProfileScreen.jsx
@@ -1,19 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import contactData from '../dummyData/contact.json'
+import Profile from '../components/ProfileScreenComponents/Profile'
 
-const ProfileScreen = (props) => {
-return (
-    <View>
-        <Text style={styles.textStyle}>ProfileScreen Page</Text>
-    </View>
-)
+const ProfileScreen = () => {
+    return (
+        <Profile {...contactData} />
+    )
 }
 
-const styles = StyleSheet.create({
-textStyle: {
-    fontSize:30,
-    margin:50
-},
-});
-
-export default ProfileScreen;
+export default ProfileScreen
