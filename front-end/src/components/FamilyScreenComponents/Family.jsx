5d6f92e9589767ff483b25be154fc6523c705fa6
naman12g <naman54g@gmail.com>
2021-07-25 00:31:30 +0530

    Start UI of Family

--- front-end/src/components/FamilyScreenComponents/Family.jsx
+++ front-end/src/components/FamilyScreenComponents/Family.jsx
@@ -1,11 +1,11 @@
-import { Box, Text } from "native-base";
+import { Box, Text, View } from "native-base";
 import React from "react";
-import { Button, View } from "react-native";
+import { Button } from "react-native";
 import { Header } from "../Utility";
 
-const Family = props => {
-    const { navigation } = props;
-    const itemSelected = props?.route?.params?.selectedItem;
+const Family = (props) => {
+  const { navigation } = props;
+  const itemSelected = props?.route?.params?.selectedItem;
   return (
     // <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
     //       <Text>Families Page {itemSelected.name}</Text>
@@ -13,13 +13,22 @@ const Family = props => {
     //   </View>
     <View>
       {/* <Header Title={itemSelected.name} /> */}
-      <Box bg={"fi.600"} width="100%" height="100%">
+      <Box bg="fi.600" width="100%" height="200%">
         <View style={{ flex: 1, alignItems: "center", marginTop: 40 }}>
-          <Text>Current Balance</Text>
-          <Text bold color={"fi.200"}>
+          <Text color="fi.100">Current Balance</Text>
+          <Text bold color="fi.200">
             Current Balance
           </Text>
         </View>
+        <Box
+          bg="fi.50"
+          // roundedTop="xl"
+          style={{ alignSelf: "flex-end", borderRadius: 50}}
+          width="100%"
+          height="90%"
+        >
+          <Text>HI</Text>
+        </Box>
       </Box>
     </View>
   );
