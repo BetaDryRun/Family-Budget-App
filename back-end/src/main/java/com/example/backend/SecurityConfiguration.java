57af7395475840e0c7b3764cce9d33558f417649
Apoorv Agrawal <apoorv.agrawal@shiksha.com>
2021-07-31 17:19:18 +0530

    Add password encoding. Expose create user end-point w/o auth

--- back-end/src/main/java/com/example/backend/SecurityConfiguration.java
+++ back-end/src/main/java/com/example/backend/SecurityConfiguration.java
@@ -3,11 +3,12 @@ package com.example.backend;
 import com.example.backend.services.UserDetailServiceCustom;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
@@ -23,15 +24,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/login").permitAll()
+        http.csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/", "static/css/*", "static/js/*", "/test").permitAll()
+                .antMatchers(HttpMethod.POST, "/user").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin();
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder(10);
     }
 
 }
