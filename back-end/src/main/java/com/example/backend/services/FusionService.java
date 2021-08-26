f8b605e96e5bda88363a15efcb89ad1f118db05c
Shivansh Narayan <shivansh.narayan1@oyorooms.com>
2021-08-27 01:26:12 +0530

    review changes

--- back-end/src/main/java/com/example/backend/services/FusionService.java
+++ back-end/src/main/java/com/example/backend/services/FusionService.java
@@ -1,6 +1,8 @@
 package com.example.backend.services;
 
 import com.example.backend.exceptions.BadRequestException;
+import com.example.backend.exchanges.CreateFusionAccountRequest;
+import com.example.backend.exchanges.CreateFusionAccountResponse;
 import com.example.backend.exchanges.DefaultResponse;
 import com.example.backend.exchanges.IssueBundleRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,22 +19,20 @@ public class FusionService {
     private static final String createUserUrl = "https://fusion.preprod.zeta.in/api/v1/ifi/140793/applications/newIndividual";
     private static final String issueBundleRequest = "https://fusion.preprod.zeta.in/api/v1/ifi/140793/bundles/1efa0950-5257-403b-a38d-61362060c565/issueBundle";
 
-    public Boolean createAccount() throws BadRequestException {
+    public CreateFusionAccountResponse createAccount(CreateFusionAccountRequest request) throws BadRequestException {
 
         try {
-
+            return restTemplate.postForObject(createUserUrl,request,CreateFusionAccountResponse.class);
         } catch (Exception e) {
-
             throw new BadRequestException("Exception in creating FUSION account");
         }
 
-        return true;
     }
 
     public Boolean issueBundle(IssueBundleRequest issueBundleRequest) throws BadRequestException {
 
         try {
-
+            return restTemplate.getForObject(issueBundleRequest,)
         } catch (Exception e) {
 
             throw new BadRequestException("Exception in issuing bundle request");
